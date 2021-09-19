4d42040093335b15beb85e81b4a7d97356ae853b
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2021-09-19 19:48:22 -0300

    quase finalizado

--- Lista_função/Função_com_struct.c
+++ Lista_função/Função_com_struct.c
@@ -1,33 +1,33 @@
 /*
 1. Elabore UM programa que permita o cadastro e a
-impressÃ£o das seguintes estruturas:
+impressão das seguintes estruturas:
 Estrutura Professor
-    cpf:atÃ© 11 caracteres (sem formataÃ§Ã£o)
-    Nome: atÃ© 30 caracteres
-    TitulaÃ§Ã£o: atÃ© 40 caracteres
+    cpf:até 11 caracteres (sem formatação)
+    Nome: até 30 caracteres
+    Titulação: até 40 caracteres
 
 Estrutura Cliente
-    CÃ³digo: tipo inteiro
-    Nome: atÃ© 30 caracteres
-    Telefone: atÃ© 12 caracteres
-    e-mail: atÃ© 40 caracteres
-2. Cada estrutura poderÃ¡ armazenar atÃ© 10 cadastros;
-3. Crie um MENU que permita acessar e cadastrar tais informaÃ§Ãµes:
+    Código: tipo inteiro
+    Nome: até 30 caracteres
+    Telefone: até 12 caracteres
+    e-mail: até 40 caracteres
+2. Cada estrutura poderá armazenar até 10 cadastros;
+3. Crie um MENU que permita acessar e cadastrar tais informações:
 1)Cadastro de Professor
 2)Cadastro de Cliente
 3)Sair
-4. Somente quando o usuÃ¡rio escolher a opÃ§Ã£o SAIR a execuÃ§Ã£o do programa
-deverÃ¡ ser finalizada.
+4. Somente quando o usuário escolher a opção SAIR a execução do programa
+deverá ser finalizada.
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#define aux 10
+#define aux 1
 
 struct professor{
     int cpf[11];
     char nome[30];
-    char titulaÃ§Ã£o[40];
+    char titulacao[40];
 }prof[aux];
 
 struct cliente{
@@ -41,13 +41,60 @@ int main(){
     setlocale(LC_ALL, "portuguese");
 
         int op;
-            printf("Quem vocÃª deseja cadastrar primeiro ?\n");
+            printf("\n\nQuem você deseja cadastrar primeiro ?\n\n");
             printf("1_Professor \t2_Cliente\t3_Sair\n");
             scanf("%d", &op);
                 while (op!=3)
                 {
-                    
+                    if(op==1){
+                        printf("\nDigite os dados do professor\n\n");
+                            for( int i=0;i<aux;i++){
+                                printf("Qual o CPF do %d° professor\n", i+1);
+                                scanf("%d", &prof[i].cpf);
+                                printf("Qual o nome do %d° professor\n", i+1);
+                                fflush(stdin);
+                                fgets(prof[i].nome,30,stdin);
+                                printf("Qual a titulação do %d° professor\n", i+1);
+                                fflush(stdin);
+                                fgets(prof[i].titulacao,40,stdin);
+                            }
+                            printf("\nCadastro concluido com sucesso\n");
+                            printf("--------------Ficha de Professores--------------\n");
+                            for( int i=0;i<aux;i++){
+                                printf("%d° professor..........%s",i+1,prof[i].nome);
+                                printf("CPF: %d\n",prof[i].cpf);
+                                printf("Titulação: %s",prof[i].titulacao);
+                            }
+                            printf("\nQuem você quer cadastrar ágora?\n1_Professor\t2_Cliente\t3_Sair\n");
+                            scanf("%d", &op);
+                    }
+                    if(op==2){
+                        printf("digite os dados do Cliente\n");
+                            for(int i=0;i<aux;i++){
+                                printf("Qual o código do %d° cliente ?\n", i+1);
+                                scanf("%d", &clie[i].codigo);
+                                printf("Qual o nome do %d° cliente\n", i+1);
+                                fflush(stdin);
+                                fgets(clie[i].nome,30,stdin);
+                                printf("Qual o telefone do %d° cliente?\n", i+1);
+                                scanf("%d", &clie[i].telefone);
+                                printf("Qual o e-mail do cliente ?\n");
+                                fflush(stdin);
+                                fgets(clie[i].email,40,stdin);
+                            }
+                            printf("\nCadastro concluido\n");
+                            printf("--------------Ficha de Clientes--------------\n");
+                            for( int i=0;i<aux;i++){
+                                printf("%d° Cliente..........%s",i+1,clie[i].nome);
+                                printf("Código: %d\n",clie[i].codigo);
+                                printf("Telefone: %d\n",clie[i].telefone);
+                                printf("E-mail: %s",clie[i].email);
+                            }
+                            printf("\nQuem você quer cadastrar ágora?\n1_Professor\t2_Cliente\t3_Sair\n");
+                            scanf("%d", &op);
+                    }
                 }
+                printf("Fim de cadastros\n\n\n");
                 
 
 }
