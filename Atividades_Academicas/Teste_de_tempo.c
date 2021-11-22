b2cafba39894dea1a5eafb755cd24cc36287f596
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2021-11-22 01:26:28 -0300

    Função organizar funcionando
    A função organizar tem o objetivo de receber um valor do arquivo .txt que estar guardado no vetor vet[aux] para desmembra-lo para que possa ser ordenado nas próximas funções.

--- Atividades_Academicas/Teste_de_tempo.c
+++ Atividades_Academicas/Teste_de_tempo.c
@@ -1,9 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<time.h>
+#include <math.h>
 #define aux 5000
 
-void ler_arquivo(int vet[]){
+void organizar(int separar){//vai receber um valor do vetor que estara guardando os valores do arquivo e desmembrar o valor
+    int num, numer[20], controle=2, j=0, x=1;
+    char valor_ordenar[20];
+
+        sprintf(valor_ordenar,"%d", separar);//converte valores inteiros em strings
+        num = strlen(valor_ordenar)-1;
+        int pot=pow(10,num);
+
+            while(x){
+                if(separar-pot<pow(10,num)){
+                    numer[j]=separar/pow(10,num);
+                    separar = separar-pot;
+                    num = num-1;
+                    pot = pow(10,num);
+                    controle=2;
+                    j++;
+                }else{
+                    pot = pow(10,num);
+                    pot = pot*controle;
+                    controle++;
+                }
+                if(j==strlen(valor_ordenar)){x=0;}
+            }
+
+}
+/*
+
+int metodo_bolha(int vet[]){
+    int temp,i, j, auxiliar;
+    int veterinario;
+    for(i=0;i<aux;i++){
+        auxiliar = vet[i];
+
+    }
+}
+
+
+int ler_arquivo(int vet[]){
     FILE *arq;
     int x, i;
         arq = fopen("numeros.txt", "r");
@@ -18,13 +57,14 @@ void ler_arquivo(int vet[]){
             }else{
                 printf("Erro com o arquivo");
             }
-}
+    return vet;
+}*/
 
 int main(){
     int vet[aux];
         ler_arquivo(vet);
 
-    for(int i=0; i<aux; i++){
-        printf("%d\n", vet[i]);
-    }
+        int teste = 654321;
+        organizar(teste);
+
 }
