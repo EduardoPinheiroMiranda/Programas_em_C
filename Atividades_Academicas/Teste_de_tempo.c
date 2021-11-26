efd3233c29d7b4e84e847f6e33ea42a19122b8c0
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2021-11-25 21:11:16 -0300

    implementação de outros metodos

--- Atividades_Academicas/Teste_de_tempo.c
+++ Atividades_Academicas/Teste_de_tempo.c
@@ -139,15 +139,118 @@ int metodo_insercao_5000(int vet[]){
     return 0;
 }
 
+int metodo_insercao_500(int vet[]){
+    int j, i, x, cont;
+    char num_string[6], temp;// num_string[6] vai receber os valores inteiros convertidos para strings.
+    
+    for(int k=0; k<500; k++){
+        sprintf(num_string, "%d", vet[k]);
+        cont = strlen(num_string);
+            for(int k=0; k<5; k++){
+                sprintf(num_string, "%d", vet[k]);
+                cont = strlen(num_string);
+        
+                for(j=1; j<cont; j++) {
+                    temp = num_string[j];
+                    i = j-1;
+            
+                    while(i>=0 && num_string[i]>temp) {
+                        num_string[i+1] = num_string[i];
+                        i--;
+                    }
+                    num_string[i+1] = temp;
+                }
+            }
+    }
+    return 0;
+}
+
+int metodo_shellsort_5000(int vet[]){
+    int j, i, h, cont;
+    char num_string[6], temp;//vai receber os valores inteiros convertidos para strings.
+    
+    for(int k=0; k<aux; k++){
+        sprintf(num_string, "%d", vet[k]);
+        cont = strlen(num_string);
+        
+        do {
+        h=(3*h) + 1;
+        } while(h<cont);
+    
+        do {
+            h/=3; //ou h = h/3
+            for(i=h;i<cont;i++) {
+                temp = num_string[i];
+                j=i-h;
+                
+                while(j>=0 && temp<num_string[j]) {
+                    num_string[j+h] = num_string[j];
+                    j-=h;
+                }
+                num_string[j+h] = temp;
+            }
+        } while(h>1);
+    
+        for(i=0;i<cont;i++) {
+            printf("%c ", num_string[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int metodo_shellsort_500(int vet[]){
+    int j, i, h, cont;
+    char num_string[6], temp;//vai receber os valores inteiros convertidos para strings.
+    
+    for(int k=0; k<500; k++){
+        sprintf(num_string, "%d", vet[k]);
+        cont = strlen(num_string);
+        
+        do {
+        h=(3*h) + 1;
+        } while(h<cont);
+    
+        do {
+            h/=3; //ou h = h/3
+            for(i=h;i<cont;i++) {
+                temp = num_string[i];
+                j=i-h;
+                
+                while(j>=0 && temp<num_string[j]) {
+                    num_string[j+h] = num_string[j];
+                    j-=h;
+                }
+                num_string[j+h] = temp;
+            }
+        } while(h>1);
+    
+        for(i=0;i<cont;i++) {
+            printf("%c ", num_string[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
 int main(){
     int vet[aux];
     clock_t BOBO;
     vet[aux]=ler_arquivo(vet);
     
     BOBO = clock();
-    metodo_insercao_5000(vet);
+    metodo_bolha_5000(vet);
     BOBO = clock() - BOBO;
-    //tempo_sel[2] = metodo_selecao(vet);
+
+    metodo_bolha_500;
+    metodo_selecao_5000;
+    metodo_selecao_500;
+    metodo_insercao_5000;
+    metodo_insercao_500;
+    metodo_shellsort_5000;
+    metodo_shellsort_500;
+    
 
     printf("%f", ((float)BOBO)/CLOCKS_PER_SEC);
     //printf("%f\n%f ", tempo_bobo[0], tempo_bobo[1]);
